2f0224fb4837ad5962cc36f145d6b1177afb040e
Omer Levi Hevroni <omerl@soluto.com>
2017-09-12 19:01:45 +0300

    run client in a loop

--- client/client.rb
+++ client/client.rb
@@ -1,39 +1,57 @@
 require 'openssl'
 require 'net/http'
-require 'jwt'
 require 'json/jwt'
+require 'SecureRandom'
 
-if (!File.exists?('key.pem'))
-    key = OpenSSL::PKey::RSA.new 2048
-    file = File.open('key.pem', 'w')
-    file.write(key.to_pem)
-    uri = URI('http://localhost:5000/api/v1/application')
-    http = Net::HTTP.new(uri.host, uri.port)
-    req = Net::HTTP::Post.new(uri.path, 'Content-Type' => 'application/json')
-    jwk = key.public_key.to_jwk
-    req.body = {
-        Id: '1', 
-        OldSyncKey: 1,
-        NewSyncKey: 2,
-        PublicKey: {
-            "kty": "RSA",
-            "e": jwk['e'],
-            "n": jwk['n']
-        }
-    }.to_json
-    res = http.request(req)
-    puts "response #{res.body}"
-else 
-    key = OpenSSL::PKey::RSA.new File.read 'key.pem'
-end
+key = OpenSSL::PKey::RSA.new 2048
+appId = SecureRandom.random_number(99999)
 
 payload = {
-    :NewSyncKey => 1,
-    :OldSyncKey => 2
+    OldSyncKey: SecureRandom.random_number(99999),
+    NewSyncKey: SecureRandom.random_number(99999)
 }
+#provision
 
-token = JSON::JWT.new(payload).sign(key, :RS256)
+uri = URI('http://localhost:5000/api/v1/application')
+http = Net::HTTP.new(uri.host, uri.port)
+req = Net::HTTP::Post.new(uri.path, 'Content-Type' => 'application/json')
+jwk = key.public_key.to_jwk
 
-uri = URI('http://localhost:5000/connect/token')
-res = Net::HTTP.post_form(uri, 'client_id' => 'ruby', 'client_secret' => 'secret', 'grant_type' => 'jwt-otp', 'scope' => 'api1', 'app-id' => '1', 'jwt' => token.to_s)
-puts res.body
+req.body = {
+    Id: appId, 
+    OldSyncKey: payload[:OldSyncKey],
+    NewSyncKey: payload[:NewSyncKey],
+    PublicKey: {
+        "kty": "RSA",
+        "e": jwk['e'],
+        "n": jwk['n']
+    }
+}.to_json
+res = http.request(req)
+
+if (!res.kind_of? Net::HTTPSuccess)
+    puts res    
+    exit(1)
+end
+
+while true 
+    puts 'requesting token'
+    payload[:OldSyncKey] = payload[:NewSyncKey]
+    payload[:NewSyncKey] = SecureRandom.random_number(99999)
+
+    token = JSON::JWT.new(payload).sign(key, :RS256)
+
+    uri = URI('http://localhost:5000/connect/token')
+    res = Net::HTTP.post_form(uri, 'client_id' => 'ruby', 'client_secret' => 'secret', 'grant_type' => 'jwt-otp', 'scope' => 'api1', 'app-id' => appId, 'jwt' => token.to_s)
+
+    if (!res.kind_of? Net::HTTPSuccess)
+        puts res.body
+        exit(1)
+    end
+
+    puts 'token recevied, press q to exit or any key to continue'
+
+    if gets.chomp == 'q'
+        exit(0)
+    end
+end 
