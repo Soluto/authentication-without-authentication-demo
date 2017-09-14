06c43324de3c76c6b8acaecff2b1ee7b3331deda
Omer Levi Hevroni <omerl@soluto.com>
2017-09-14 08:13:08 +0300

    add failure resistence

--- client/client.rb
+++ client/client.rb
@@ -13,7 +13,7 @@ payload = {
 }
 #provision
 
-uri = URI('http://localhost:5000/api/v1/application')
+uri = URI('http://localhost:8081/api/v1/application')
 http = Net::HTTP.new(uri.host, uri.port)
 req = Net::HTTP::Post.new(uri.path, 'Content-Type' => 'application/json')
 jwk = key.public_key.to_jwk
@@ -36,22 +36,34 @@ if (!res.kind_of? Net::HTTPSuccess)
 end
 
 while true 
+    shouldRoll = true
     puts 'requesting token'
     payload[:OldSyncKey] = payload[:NewSyncKey]
     payload[:NewSyncKey] = SecureRandom.random_number(99999)
 
     token = JSON::JWT.new(payload).sign(key, :RS256)
 
-    uri = URI('http://localhost:5000/connect/token')
+    uri = URI('http://localhost:8081/connect/token')
     res = Net::HTTP.post_form(uri, 'client_id' => 'ruby', 'client_secret' => 'secret', 'grant_type' => 'jwt-otp', 'scope' => 'sensitive.read', 'app-id' => appId, 'jwt' => token.to_s)
 
     if (!res.kind_of? Net::HTTPSuccess)
-        puts res.body
-        exit(1)
+        if (res.kind_of? Net::HTTPBadRequest)
+            puts "bad request"
+            shouldRoll = false
+        else
+            puts "failed to get token"
+            puts res
+        end
+
+        next
     end
+    
+    shouldRoll = true
     token = JSON.parse(res.body)['access_token']
 
-    uri = URI('http://localhost:4000/api/v1/sensitive')
+    puts 'token received'
+
+    uri = URI('http://localhost:8082/api/v1/sensitive')
     req = Net::HTTP::Get.new(uri)    
     req['Authorization'] = "Bearer #{token}"
     http = Net::HTTP.new(uri.host, uri.port)    
